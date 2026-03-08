248fb131b83003095d1a8edb2f12b68c31f9bfe8
jad ghribi <jadghribi@SS02061.ADDIXO.LOCAL>
2026-03-08 12:47:10 +0100

    pipeline add test v4

--- spring-boot-microservices-angular/backend/answer-service/src/test/java/com/microservices/examenservice/ExamenServiceApplicationTests.java
+++ spring-boot-microservices-angular/backend/answer-service/src/test/java/com/microservices/examenservice/ExamenServiceApplicationTests.java
@@ -3,11 +3,11 @@ package com.microservices.examenservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ExamenServiceApplication.class)
+@SpringBootTest
 class ExamenServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
