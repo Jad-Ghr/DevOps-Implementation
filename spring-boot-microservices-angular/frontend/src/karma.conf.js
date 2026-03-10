63ba4401b621fff4bed55a7ee967ae1366f4ee5b
jad ghribi <jadghribi@SS02061.ADDIXO.LOCAL>
2026-03-11 00:25:58 +0100

    pipeline add test v14

--- spring-boot-microservices-angular/frontend/src/karma.conf.js
+++ spring-boot-microservices-angular/frontend/src/karma.conf.js
@@ -9,24 +9,27 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('karma-junit-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, './coverage/my-app'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
     },
     junitReporter: {
       outputDir: 'test-results',
       outputFile: 'junit-report.xml',
       suite: ''
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
+    reporters: ['progress', 'kjhtml', 'coverage', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
